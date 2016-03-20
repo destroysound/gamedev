4172ac0dca22089fcfbc0d05cf6640e6cc234967
destroysound <whunt.it@gmail.com>
2016-03-20 19:12:06 -0400

    Add player start position to tilemap.

--- public/js/game.js
+++ public/js/game.js
@@ -1,150 +1,78 @@
-var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
+    game.load.tilemap('castletiles', 'assets/tilemaps/castle.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('castle', 'assets/tilemaps/tilesets/Castle.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 }
 
+var map;
+var terrain;
+var buildings;
+var decorations;
+
 var player;
-var platforms;
+
 var cursors;
 
-var stars;
-var score = 0;
-var scoreText;
-
 function create() {
+  
+    game.stage.backgroundColor = '#787878';
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //  The 'mario' key here is the Loader key given in game.load.tilemap
+    map = game.add.tilemap('castletiles');
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+    //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
+    //  The second parameter maps this name to the Phaser.Cache key 'tiles'
+    map.addTilesetImage('Castle', 'castle');
+    
+    //  Creates a layer from the World1 layer in the map data.
+    //  A Layer is effectively like a Phaser.Sprite, so is added to the display list.
+    terrain = map.createLayer('Terrain');
+    buildings = map.createLayer('Buildings');
+    decorations = map.createLayer('Decoration');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+    //  This resizes the game world to match the layer dimensions
+    terrain.resizeWorld();
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+    player = game.add.sprite(48, 48, 'dude');
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    var respawn = game.add.group();
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+    map.createFromObjects('Objects', 101, '', 0, true, false, respawn);
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+    respawn.forEach(function (p) {
+        console.log(p);
+        player.reset(p.x, p.y);
+    });
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
+    game.camera.follow(player);
 
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  We need to enable physics on the player
     game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.collideWorldBounds = true;
-    player.movementSpeed = 2.5;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
-    
 }
 
 function update() {
 
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.collide(player, buildings);
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    player.body.velocity.y = 0; 
+    player.body.velocity.x = 0; 
 
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150 * player.movementSpeed;
-
-        player.animations.play('left');
+    if (cursors.up.isDown) {
+        player.body.velocity.y = -150;
     }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150 * player.movementSpeed;
-
-        player.animations.play('right');
+    else if (cursors.down.isDown) {
+        player.body.velocity.y = 150;
     }
-    if (cursors.up.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.y = -150 * player.movementSpeed;
-
-        player.animations.play('left');
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150;
     }
-    else if (cursors.down.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.y = 150 * player.movementSpeed;
-
-        player.animations.play('right');
+    else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
     }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
-    }
-    
-}
-
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
 
 }
-
-
